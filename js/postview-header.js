6d2a1e7671c5f875fdc1eda3ce62ebb82aa4dd3b
gabbe-k <44163424+gabbe-k@users.noreply.github.com>
2019-01-30 12:37:23 +0100

    minor alignment

--- js/postview-header.js
+++ js/postview-header.js
@@ -1,5 +1,5 @@
 
-  function searchq() {
+  function searchSit() {
 
     var searchTxt = $("input[name='tagSearch']").val();
     $('#tagSearch-Preview').addClass('animatable--now');
@@ -21,6 +21,24 @@
 
   }
 
-  function addtag() {
+function searchSym() {
 
-  }
+  var searchTxt = $("input[name='tagSearch']").val();
+  $('#tagSearch-Preview').addClass('animatable--now');
+
+  $.post("../sql/sqscantags.php",  {searchVal: searchTxt}, function(output) {
+
+    if (output == "") {
+      $('.tagSearch-Wrapper').removeClass('tagSearch-Wrapper-Border');
+      $('#tagSearch-Preview').removeClass('animatable--now');
+    }
+    else {
+      $('.tagSearch-Wrapper').addClass('tagSearch-Wrapper-Border');
+    }
+
+
+    $("#tagSearch-Preview").html(output);
+
+  });
+
+}
