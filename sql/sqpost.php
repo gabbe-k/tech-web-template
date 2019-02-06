aa4c2452b31bb0ed2e8765a75ee16f5c5d322311
gabbe-k <44163424+gabbe-k@users.noreply.github.com>
2019-02-06 15:01:57 +0100

    array fixed

--- sql/sqpost.php
+++ sql/sqpost.php
@@ -1,4 +1,43 @@
 <?php
+
+  function InsertTag($tagArrArr, $conn, $string, $postId) {
+
+    $tagArr = array();
+
+      for ($j=0; $j < 3; $j++) {
+
+        for ($i=0; $i < 1; $i++) {
+
+          echo $tagArrArr[$j][0];
+
+          $result = mysqli_query($conn, "SELECT * FROM tags WHERE tagText='$tagArrArr[$j][0]'");
+          $resultLen = mysqli_num_rows($result);
+
+          if ($resultLen > 0) {
+
+              $row = mysqli_fetch_assoc($result);
+              $tagArr[$j] = $row['tagText'];
+              //$conn->query("INSERT INTO posttag (postId, '$string') VALUES ('$postId', '')");
+
+          }
+          else {
+
+            $tagId = DupeSearch($conn, "tags", "tagId");
+            $tagArr[$j] = $row['tagText'];
+            $conn->query("INSERT INTO tags (tagText, tagId) VALUES ('$tagArrArr[$j][$i]', '$tagId')");
+
+          }
+
+        }
+
+      }
+
+    $conn->query("INSERT INTO posttag (postId, situationTagId, symptomTagId, modelTagId) VALUES ('$postId', '$tagArr[0]', '$tagArr[1]', '$tagArr[2]')");
+
+  }
+
+
+
   require_once('sqconnect.php');
   require_once('data_valid.php');
 
@@ -12,33 +51,26 @@
   $description = ClearTags($conn, $_POST['description']);
   $title = ClearTags($conn, $_POST['title']);
 
-  $postId = DupeSearch($conn, "comments", "commId");
+  $situation = ClearTags($conn, $_POST['situation']);
+  $symptom = ClearTags($conn, $_POST['symptom']);
+  $model = ClearTags($conn, $_POST['model']);
 
-  $tags = ClearTags($conn, $_POST['tags']);
+  $postId = DupeSearch($conn, "posts", "postId");
 
-  $tagsArray = explode(',', $tags);
+  $situationArr = explode(',', $situation);
+  $symptomArr = explode(',', $symptom);
+  $modelArr = explode(',', $model);
 
-  for ($i=0; $i < count($tagsArray); $i++) {
+  $tagArrArr=array
+    (
+      $situationArr,
+      $symptomArr,
+      $modelArr
+    );
 
-    $result = mysqli_query($conn, "SELECT * FROM tags WHERE tagText='$tagsArray[$i]'");
-    $resultLen = mysqli_num_rows($result);
+    print_r($tagArrArr);
 
-    if ($resultLen > 0) {
-
-        $row = mysqli_fetch_assoc($result);
-        $conn->query("INSERT INTO posttag (postId, tagId) VALUES ('$postId', '$row[tagId]')");
-
-
-    }
-    else {
-
-      $tagId = rand(0, 9999);
-      $conn->query("INSERT INTO posttag (postId, tagId) VALUES ('$postId', '$tagId')");
-      $conn->query("INSERT INTO tags (tagText, tagId) VALUES ('$tagsArray[$i]', '$tagId')");
-
-    }
-
-  }
+  InsertTag($tagArrArr, $conn, "situationTagId", $postId);
 
   $conn->query("INSERT INTO posts (id, titleText, postText, postId) VALUES ('$id', '$title', '$description', '$postId')");
 
--- view/form/ask.php
+++ view/form/ask.php
@@ -5,7 +5,9 @@
   <form action="sql/sqpost.php"  id="loginForm" method="post">
     <input type="text" id="formfields" name="title" value="Title">
     <input type="text" id="formfields" name="description" value="Description">
-    <input id="formfields" type="text" name="tags" value="Tags, separate with ','">
+    <input id="formfields" type="text" name="situation" value="Tags, separate with ','">
+    <input id="formfields" type="text" name="symptom" value="Tags, separate with ','">
+    <input id="formfields" type="text" name="model" value="Tags, separate with ','">
     <input type="submit" id="submitbutton" value="Submit">
   </form>
 
